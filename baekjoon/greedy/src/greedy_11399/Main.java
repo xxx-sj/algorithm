682a3cac435417098f2a8755b69e70bd53c24226
loky13 <loky13@daum.net>
2023-07-22 21:49:15 +0900

    그리디 알고리즘 11399

---
+++ baekjoon/greedy/src/greedy_11399/Main.java
@@ -0,0 +1,34 @@
+package greedy_11399;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] time = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            time[i] = sc.nextInt();
+        }
+
+        Arrays.sort(time);
+
+        int prev = 0;
+        int sum = 0;
+
+        for(int i = 0; i < N; i++) {
+            prev += time[i];
+
+            sum += prev;
+        }
+
+        System.out.println(sum);
+
+
+    }
+}
