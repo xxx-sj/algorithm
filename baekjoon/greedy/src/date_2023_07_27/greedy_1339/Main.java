ff0541a720aaabeb68e8ed334bbe6d3bb38fa109
loky13 <loky13@daum.net>
2023-07-30 15:34:21 +0900

    그리디 알고리즘 1339

---
+++ baekjoon/greedy/src/date_2023_07_27/greedy_1339/Main.java
@@ -0,0 +1,42 @@
+package date_2023_07_27.greedy_1339;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] alpha_arr = new int[26];
+
+        for(int i = 0; i < N; i++) {
+            String alpha = br.readLine();
+            int base = 10;
+            for(int j = 1; j <= alpha.length(); j++) {
+                char c = alpha.charAt(j - 1);
+                alpha_arr[c - 'A'] += Math.pow(10, (alpha.length() - j));
+            }
+        }
+
+        Arrays.sort(alpha_arr);
+
+
+
+        int result = 0;
+        int number = 9;
+        for(int i = alpha_arr.length - 1; i >= 0; i--) {
+            if (alpha_arr[i] == 0) {
+                break;
+            }
+
+            result += alpha_arr[i] * number;
+            number--;
+        }
+
+        System.out.println(result);
+
+    }
+}
