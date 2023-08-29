ea3b73b5b71b9ab76492d2391f368cc423874b75
loky13 <loky13@daum.net>
2023-08-29 16:09:06 +0900

    DP algorithm 10844 설명추가

--- baekjoon/DP/src/date_2023_08_15/DP_10844/Main.java
+++ baekjoon/DP/src/date_2023_08_15/DP_10844/Main.java
@@ -22,10 +22,15 @@ public class Main {
         }
 
         long result = 0;
+        bottom_up(N);
 
         for(int i = 1; i <= 9; i++) {
-            result += recur(N, i);
+            result += dp[N][i];
         }
+
+//        for(int i = 1; i <= 9; i++) {
+//            result += recur(N, i);
+//        }
 
         System.out.println(result % mod);
 
@@ -49,5 +54,21 @@ public class Main {
         }
 
         return dp[digit][val];
+    }
+
+    static void bottom_up(int N) {
+
+        for(int i = 2; i <= N; i++) {
+
+            for(int j = 0; j < 10; j++) {
+                if(j == 0) {
+                    dp[i][j] = dp[i - 1][1] % mod;
+                } else if (j == 9) {
+                    dp[i][j] = dp[i - 1][8] % mod;
+                } else {
+                    dp[i][j] = (dp[i - 1][j + 1] + dp[i -1][j - 1]) % mod;
+                }
+            }
+        }
     }
 }
