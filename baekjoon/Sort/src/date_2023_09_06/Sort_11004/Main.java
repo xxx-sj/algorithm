5154ae3b9804136fda37530ef59f3de0550a94c2
loky13 <loky13@daum.net>
2023-09-06 23:40:59 +0900

    sort 11004

---
+++ baekjoon/Sort/src/date_2023_09_06/Sort_11004/Main.java
@@ -0,0 +1,28 @@
+package date_2023_09_06.Sort_11004;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        long[] arr = new long[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+
+
+    }
+}
